aff2b37daa671591c4bf772ec6850c15f6661078
JaeBeen95 <nin2348@gmail.com>
2025-03-18 00:34:27 +0900

    ✨ feat: 라우트 정보 배열 추가 및 모듈화

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,14 @@
 import { ARTICLE_LIST, ARTICLE_DETAIL } from './mock'
 import { articleView } from './ui/view/articleView'
 import { articleDetailView } from './ui/view/articleDetailView'
+import { addRoute, navigate, router, setDefaultRoute } from './router'
 import { handleCategoryClick } from './ui/logic/handleCategoryClick'
 import './style.css'
 
-function router(): void {
-  const path = window.location.pathname
-
-  if (path === '/' || path === '') {
-    articleView('root', ARTICLE_LIST)
-  } else {
-    articleDetailView('root', ARTICLE_DETAIL)
-  }
-}
-
-function navigate(path: string, updateView: boolean = true): void {
-  history.pushState({}, '', path)
-  if (updateView) {
-    router()
-  }
-}
-
 window.navigate = navigate
-document.addEventListener('popstate', router)
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', () => {
+  setDefaultRoute(() => articleView('root', ARTICLE_LIST))
+  addRoute('/article', () => articleDetailView('root', ARTICLE_DETAIL))
   router()
   handleCategoryClick()
 })
